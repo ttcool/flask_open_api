2c6d5ba4a640aaa6ab31d50f94b2a372ee70aa5c
unknown <ttcool@live.cn>
2017-08-21 10:55:57 +0800

    add app.py requirement.txt

--- app.py
+++ app.py
@@ -1,5 +1,62 @@
+# coding:utf-8
+
 from flask import Flask,jsonify,make_response,abort,request
+from flask.ext.sqlalchemy import SQLAlchemy
+from marshmallow import Schema
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'daferr33354gfdsgrfhgh'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://usename:passwd@ip_address/acct'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+db = SQLAlchemy(app)
+
+class Article(db.Model):
+    __tablename__ = 'articles'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text(), nullable=False)
+
+    def __unicode__(self):
+        return self.content
+
+# we use marshmallow Schema to serialize our articles
+class ArticleSchema(Schema):
+    """
+    Article dict serializer
+    """
+    class Meta:
+        # which fields should be serialized?
+        fields = ('id', 'title', 'content')
+
+article_schema = ArticleSchema()
+# many -> allow for object list dump
+articles_schema = ArticleSchema(many=True)
+
+@app.route("/articles/", methods=["GET"])
+@app.route("/articles/<article_id>", methods=["GET"])
+def articles(article_id=None):
+    if article_id:
+        article = Article.query.get(article_id)
+        if article is None:
+            return jsonify({"msgs": ["the article you're looking for could not be found"]}), 404
+
+        result = article_schema.dump(article)
+        return jsonify({'article': result})
+    else:
+        # never return the whole set! As it would be very slow
+        queryset = Article.query.limit(10)
+        result = articles_schema.dump(queryset)
+        # jsonify serializes our dict into a proper flask response
+        return jsonify({"articles": result.data})
+
+db.create_all()
+# let's populate our database with some data; empty examples are not that cool
+if Article.query.count() == 0:
+    article_a = Article(title='some title', content='somecontent')
+    article_b = Article(title='other title', content='othercontent')
+    db.session.add(article_a)
+    db.session.add(article_b)
+    db.session.commit()
 
 users = [
     {
@@ -62,4 +119,7 @@ def delete_user(id):
     return jsonify({}), 204
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # we define the debug environment only if running through command line
+    app.config['SQLALCHEMY_ECHO'] = True
+    app.debug = True
+    app.run()
