4b9716106eb0764f4020d6d37946c27ec1f2ac25
unknown <ttcool@live.cn>
2017-09-01 10:28:06 +0800

    add manager

--- manage.py
+++ manage.py
@@ -3,11 +3,11 @@
 from flask_script import Manager,Server
 from main import app,db,Article
 
-manger=Manager(app)
-manger.add_command("server",Server)
-@manger.shell
+manager=Manager(app)
+manager.add_command("server",Server)
+@manager.shell
 def make_shell_context():
     return dict(app=app,db=db,Article=Article)
 
 if __name__=="__main__":
-    manger.run()
+    manager.run()
