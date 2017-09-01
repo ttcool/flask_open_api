62d03c49745b1547c0ac33efa1cf726d7e8e9217
unknown <ttcool@live.cn>
2017-09-01 11:07:42 +0800

    update manage

--- manage.py
+++ manage.py
@@ -1,10 +1,14 @@
 # coding:utf-8
 
 from flask_script import Manager,Server
+from flask_migrate import Migrate,MigrateCommand
 from main import app,db,Article
 
+migrate=Migrate(app,db)
 manager=Manager(app)
 manager.add_command("server",Server)
+manager.add_command("db",MigrateCommand)
+
 @manager.shell
 def make_shell_context():
     return dict(app=app,db=db,Article=Article)
